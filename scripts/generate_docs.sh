a5b48cce65e56b18852a4cc191201e62cb4da02e
Sean P. Cornelius <spcornelius@gmail.com>
2019-05-03 19:00:21 -0400

    Improve formatting of sphinx docs

--- scripts/generate_docs.sh
+++ scripts/generate_docs.sh
@@ -9,21 +9,26 @@ PKG=$(find . -maxdepth 2 -name __init__.py -print0 | xargs -0 -n1 dirname | xarg
 AUTHOR=$(head -n 1 AUTHORS)
 sphinx-apidoc --full --force -A "$AUTHOR" --module-first --doc-version=$(python setup.py --version) -F -o docs $PKG/ $(find . -type d -name tests)
 cat <<EOF >>docs/index.rst
+
 Overview
 ========
-
 .. mdinclude:: README.md
 
 EOF
 MATCH="'sphinx.ext.viewcode'"
 NEW="'sphinx.ext.viewcode', 'sphinx.ext.autosummary', 'numpydoc', 'm2r'"
 sed -i "" "s/$MATCH/$NEW/g" docs/conf.py
-#sed -i "" "s/alabaster/sphinx_rtd_theme/g" docs/conf.py
+sed -i "" "s/alabaster/sphinx_rtd_theme/g" docs/conf.py
 
+echo "autoclass_content = 'both'" >> docs/conf.py # include both class docstring and __init__
+echo "autodoc_default_flags = ['members', 'show-inheritance']" >> docs/conf.py
+echo "autosummary_generate = True" >> docs/conf.py
 echo "numpydoc_class_members_toctree = False" >>docs/conf.py
+echo "numpydoc_attributes_as_param_list = False" >>docs/conf.py
+echo "pygments_style = 'sphinx'" >>docs/conf.py
+
 ABS_REPO_PATH=$(unset CDPATH && cd "$(dirname "$0")/.." && echo $PWD)
 if [[ ! -d docs/_build/html ]]; then
     mkdir docs/_build/html
 fi
-
 ( cd docs; PYTHONPATH=$ABS_REPO_PATH make html >_build/html/build.log )
