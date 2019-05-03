a810485b36934c887df539c4506a47df83cb8fc7
Sean P. Cornelius <spcornelius@gmail.com>
2019-05-03 13:27:12 -0400

    Fix extras_req

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ version = '0.1.0'
 
 extras_req = {
     'testing': ['pytest', 'pytest-pep8'],
-    'docs': ['sphinx', 'sphinx_rtd', 'theme numpydoc']
+    'docs': ['sphinx', 'sphinx_rtd_theme', 'numpydoc']
 }
 
 extras_req['all'] = list(chain(*extras_req.values()))
