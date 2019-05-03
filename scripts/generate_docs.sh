947f07d4e69402b1ec8af62bee72231aaa7a7aaf
Sean P. Cornelius <spcornelius@gmail.com>
2019-05-03 12:55:30 -0400

    Add script for doc generation

---
+++ scripts/generate_docs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -xe
+#
+# Usage:
+#
+#    $ ./scripts/generate_docs.sh
+#
+NARGS=$#
+PKG=$(find . -maxdepth 2 -name __init__.py -print0 | xargs -0 -n1 dirname | xargs basename)
+AUTHOR=$(head -n 1 AUTHORS)
+sphinx-apidoc --full --force -A "$AUTHOR" --module-first --doc-version=$(python setup.py --version) -F -o docs $PKG/ $(find . -type d -name tests)
+cat <<EOF >>docs/index.rst
+Overview
+========
+$(tail -n+3 README.md)
+EOF
+MATCH="'sphinx.ext.viewcode'"
+NEW="'sphinx.ext.viewcode', 'sphinx.ext.autosummary', 'numpydoc'"
+sed -i "" "s/$MATCH/$NEW/g" docs/conf.py
+sed -i "" "s/alabaster/sphinx_rtd_theme/g" docs/conf.py
+
+echo "numpydoc_class_members_toctree = False" >>docs/conf.py
+ABS_REPO_PATH=$(unset CDPATH && cd "$(dirname "$0")/.." && echo $PWD)
+if [[ ! -d docs/_build/html ]]; then
+    mkdir docs/_build/html
+fi
+
+( cd docs; PYTHONPATH=$ABS_REPO_PATH make html >_build/html/build.log )
