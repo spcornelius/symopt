b35c940ef10ee553f9781acf7312b84c8f668fd1
Sean P. Cornelius <spcornelius@gmail.com>
2019-05-09 02:27:09 -0400

    Improve maintainability of setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,15 @@ from distutils.core import setup
 from itertools import chain
 
 import setuptools
+import re
 
 pkg_name = 'sympot'
 license = 'MIT'
-version = '0.2.0'
+url = "https://github.com/spcornelius/symopt"
+author = "Sean P. Cornelius"
+author_email = "spcornelius@gmail.com"
+version_file = "%s/_version.py" % (pkg_name,)
+readme_file = "README.md"
 
 extras_req = {
     'testing': ['pytest', 'pytest-pep8'],
@@ -25,7 +30,16 @@ classifiers = [
     "Topic :: Scientific/Engineering :: Mathematics",
 ]
 
-with open("README.md", "r") as f:
+match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                  open(version_file, "rt").read(),
+                  re.M)
+if match:
+    version = match.group(1)
+else:
+    raise RuntimeError(
+        "Unable to find version string in %s." % (version_file,))
+
+with open(readme_file, "r") as f:
     long_description = f.read()
 
 if __name__ == '__main__':
@@ -35,10 +49,10 @@ if __name__ == '__main__':
         description="Easy (non)linear optimization in Python with "
                     "symbolically-defined objective/constraints",
         long_description=long_description,
-        long_description_content_type="text/x-rst",
-        author="Sean P. Cornelius",
-        author_email="spcornelius@gmail.com",
-        url="https://github.com/spcornelius/symopt",
+        long_description_content_type="text/markdown",
+        author=author,
+        author_email=author_email,
+        url=url,
         license=license,
         packages=setuptools.find_packages(),
         install_requires=['numpy', 'scipy', 'sympy', 'orderedset'],
